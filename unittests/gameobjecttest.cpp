d29bd4d5e98627c0ce8c7a2dfd97eac4bc5651a8
TheAkss999 Laptop <aksfkb@mst.com>
2011-09-09 13:47:19 -0500

    more tests

--- unittests/gameobjecttest.cpp
+++ unittests/gameobjecttest.cpp
@@ -38,9 +38,70 @@ class foh_GOComponentB: public GOComponent
 		const GOC_IDType constructHash() const { return familyID(); }
 };
 
+
+/** components with different families **/
+class foh_GOComponentC: public GOComponent
+{
+	public:
+
+		const GOC_IDType componentID() const { return "componentC"; }
+		const GOC_IDType familyID() const { return "family2"; }
+
+		void update() {}
+
+		const GOC_IDType constructHash() const { return familyID(); }
+};
+
+/** components with different families **/
+class foh_GOComponentD: public GOComponent
+{
+	public:
+
+		const GOC_IDType componentID() const { return "componentD"; }
+		const GOC_IDType familyID() const { return "family3"; }
+
+		void update() {}
+
+		const GOC_IDType constructHash() const { return familyID(); }
+};
+
+/** components with different families **/
+class foh_GOComponentE: public GOComponent
+{
+	public:
+
+		const GOC_IDType componentID() const { return "componentE"; }
+		const GOC_IDType familyID() const { return "family4"; }
+
+		void update() {}
+
+		const GOC_IDType constructHash() const { return familyID(); }
+};
+
+
 void GameObjectTest::AddComponent()
 {
 	GameObject GO(0);
+	CPPUNIT_ASSERT( !GO.hasComponentFam("") ); // empty family check
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family1") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family2") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family3") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family4") );
+
+	//insert the first component
+	foh_GOComponentA comp1;
+	GO.setGOC( &comp1 );
+
+	CPPUNIT_ASSERT( !GO.hasComponentFam("") ); // empty family check
+	CPPUNIT_ASSERT( GO.hasComponentFam("family1") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family2") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family3") );
+	CPPUNIT_ASSERT( !GO.hasComponentFam("family4") );
+
+	CPPUNIT_ASSERT( GO.getGOC("family1") == comp1 );
+
+
+	//insert a second component
 
 }
 
